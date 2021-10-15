0bb90134c30e3d04caa1772e409bde3408d009a8
JadTawbi <jad.tawbi2@gmail.com>
2021-10-15 16:53:52 +0200

    We make hand with sphere collider

---
+++ Assets/XRGrabInteractor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Yes : MonoBehaviour
+{
+    XRGrabInteractable interactable;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
