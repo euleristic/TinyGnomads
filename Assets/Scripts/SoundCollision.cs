9bb8fe628ce407173a7d61e54c2e3a08996c346a
leowognum <leo.wognum@gmail.com>
2021-12-02 15:45:29 +0100

    Added sound and collision script
    Added sound and collision script. Different sound based on what hit it.

--- Assets/Scripts/SoundCollision.cs
+++ Assets/Scripts/SoundCollision.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class SoundCollision : MonoBehaviour
 {
-    AudioSource audioSource;
+    public AudioSource[] audioSource;
+    private bool done;
 
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        done = false;
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(1);
+        done = true;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.relativeVelocity.magnitude > 0.25f)  
-            audioSource.Play();
+
+        if(collision.rigidbody == null && done == true)
+        {
+            // Low Sound
+
+            float rand = Random.Range(0.8f, 1.2f);
+
+            audioSource[0].pitch = rand;
+
+            audioSource[0].Play();
+        }
+
+        if(collision.rigidbody != null && collision.relativeVelocity.sqrMagnitude > 0.01f && done == true)
+        {
+            // High Sound
+
+            float rand = Random.Range(0.8f, 1.2f);
+
+            audioSource[1].pitch = rand;
+            audioSource[1].Play();
+        }
     }
 }
