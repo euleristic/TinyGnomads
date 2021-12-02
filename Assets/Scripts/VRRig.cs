78a20c6088c301e35de2dcdc11812e5e417af870
cheddarjedi <sandstedtaxel@gmail.com>
2021-12-02 16:38:04 +0100

    some slight updates to VRRig script

--- Assets/Scripts/VRRig.cs
+++ Assets/Scripts/VRRig.cs
@@ -36,10 +36,10 @@ public class VRRig : MonoBehaviour
     }
 
 
-    void LateUpdate()
+    void FixUpdate()
     {
         transform.position = headConstraint.position + headBodyOffset;
-        transform.forward = Vector3.Lerp(transform.forward,Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized,Time.deltaTime * turnSmoothness);
+        transform.forward = Vector3.Lerp(transform.forward,Vector3.ProjectOnPlane(headConstraint.up,Vector3.up).normalized,Time.deltaTime * turnSmoothness);
 
         head.Map();
         leftHand.Map();
