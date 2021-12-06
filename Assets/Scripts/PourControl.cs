f054b715a8c3bc9b8fe818a023db8746e111f216
euleristic <valter.ottenvang@gmail.com>
2021-12-06 14:42:36 +0100

    Fixed pouring a bit

--- Assets/Scripts/PourControl.cs
+++ Assets/Scripts/PourControl.cs
@@ -6,19 +6,19 @@ public class PourControl : MonoBehaviour
 {
     [SerializeField] Transform top, bottom;
     [SerializeField] ParticleSystem system;
-    bool active; //I'm scared that SetActive() might be expensive, so just to be safe...
+    bool active;
 
     void Update()
     {
         if (!active && top.position.y < bottom.position.y)
         {
             active = true;
-            system.gameObject.SetActive(active);
+            system.Play();
         }
         else if (active && bottom.position.y < top.position.y)
         {
             active = false;
-            system.gameObject.SetActive(active);
+            system.Stop();
         }
     }
 }
