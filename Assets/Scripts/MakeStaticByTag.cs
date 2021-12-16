b0dcad35a8e7a323ac288d3ff9bbf8950f13dbdf
euleristic <valter.ottenvang@gmail.com>
2021-12-16 15:10:33 +0100

    Added InputDeviceManager

---
+++ Assets/Scripts/MakeStaticByTag.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeStaticByTag : MonoBehaviour
+{
+    [Header("OBS! All other gameobjects with this tag will be destroyed")]
+    [SerializeField] bool IUnderstand;
+    void Awake()
+    {
+        if (!IUnderstand) return;
+        GameObject[] objs = GameObject.FindGameObjectsWithTag(transform.tag);
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
